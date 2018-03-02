7743a151512ae632181ab76cb30d01be77aa2662
EigenMania <adrianje77@gmail.com>
2018-03-02 21:21:42 +0100

    Initial commit of main file with LS solution implemented

---
+++ main.py
@@ -0,0 +1,26 @@
+#################################
+#          IML_TO               #
+#################################
+#
+# File Name: main.py
+# Course: 252-0220-00L Introduction to Machine Learning
+#
+# Authors: Adrian Esser (aesser@student.ethz.ch)
+#          Abdelrahman-Shalaby (shalabya@student.ethz.ch)
+
+import numpy as np
+import sys
+import os
+import csv
+
+# Import training data
+data = np.genfromtxt('train.csv', delimiter=',')
+data = np.delete(data, 0, 0)
+data = np.matrix(data)
+
+A = data[:,2:] # get third column to end
+b = data[:,1] # get first column
+
+theta = np.linalg.inv(A.T * A)*A.T*b
+print(theta) # unsurprisingly, all values are 0.1
+
